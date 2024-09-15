627c9dadb096e6413c0b5837ad1ed3058673db1c
Brennan Au <brennanau@cmail.carleton.ca>
2024-09-15 19:12:44 -0400

    Added a main function that prints "Address Book"

---
+++ src/AddressBook.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class AddressBook {
+    List<BuddyInfo> listOfBuddyInfo = new ArrayList<BuddyInfo>();
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
