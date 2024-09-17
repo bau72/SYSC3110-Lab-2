efc27032bc1b6c28b069464ae9115d2cea079728
Brennan Au <brennanau@cmail.carleton.ca>
2024-09-16 21:51:38 -0400

    Added a main function that prints "Address Book" and addBuddy/removeBuddy methods

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -5,5 +5,15 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 613);
+        AddressBook addressBook = new AddressBook();
+    }
+
+    private void addBuddy(BuddyInfo buddy) {
+        listOfBuddyInfo.add(buddy);
+    }
+
+    private void removeBuddy(BuddyInfo buddy) {
+        listOfBuddyInfo.remove(buddy);
     }
 }
